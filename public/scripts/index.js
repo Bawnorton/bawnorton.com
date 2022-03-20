8c8b84fedb20111a7714f0b0a042dfce1f61d405
bawnorton <bawnorton@me.com>
2022-03-19 17:15:34 -0700

    Added Second Page

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -4,6 +4,14 @@ document.addEventListener('DOMContentLoaded', () => {
     const one = document.getElementsByClassName("one")[0]
     const two = document.getElementsByClassName("two")[0]
     const three = document.getElementsByClassName("three")[0]
+    const downArrows = document.getElementsByClassName("downarrow")
+
+    const page1 = document.getElementById("page1")
+    const page2 = document.getElementById("page2")
+    const page1title = document.getElementById("page1title")
+    const page2title = document.getElementById("page2title")
+    const page1anchor = document.getElementById("page1anchor")
+    const page2anchor = document.getElementById("page2anchor")
 
     let sideMenu = false
 
@@ -37,4 +45,35 @@ document.addEventListener('DOMContentLoaded', () => {
         }
         sideMenu = !sideMenu;
     })
+
+    page1anchor.addEventListener("click", () => {
+        page2.style.display = "none"
+        page1anchor.style.display = "none"
+        page1title.style.display = "none"
+        page1.style.display = "block"
+        page2anchor.style.display = "block"
+        page2title.style.display = "block"
+    })
+
+    page2anchor.addEventListener("click", () => {
+        page2.style.display = "block"
+        page1anchor.style.display = "block"
+        page1title.style.display = "block"
+        page1.style.display = "none"
+        page2anchor.style.display = "none"
+        page2title.style.display = "none"
+    })
+
+    for(let downArrow of downArrows) {
+        setInterval(() => {
+            downArrow.style.bottom = '3vh'
+            setTimeout(() => {
+                downArrow.style.bottom = '5vh'
+            }, 1250)
+        }, 2500)
+
+        document.addEventListener("scroll",  () => {
+            downArrow.style.opacity = 1 - (window.scrollY * 6 / window.innerHeight)
+        })
+    }
 })
